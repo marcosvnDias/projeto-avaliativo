47358b3ab61f580a69607165dcf65759c484b507
Marcos Vinicios <mvdias1268@gmail.com>
2024-04-11 23:30:58 -0300

    Enviando os arquivos para o github

---
+++ src/main/java/com/senai/fullstack/projetoavaliativo/ProjetoAvaliativoApplication.java
@@ -0,0 +1,13 @@
+package com.senai.fullstack.projetoavaliativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAvaliativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAvaliativoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/fullstack/projetoavaliativo/ProjetoAvaliativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.fullstack.projetoavaliativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAvaliativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
